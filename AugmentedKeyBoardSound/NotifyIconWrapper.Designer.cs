1c22fe8c8ab3fec5f252b1b60960d42bc06002c9
quista <brave.heaven@gmail.com>
2017-12-13 21:46:20 +0900

    first commit

---
+++ AugmentedKeyBoardSound/NotifyIconWrapper.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace AugmentedKeyBoardSound
+{
+    partial class NotifyIconWrapper
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region コンポーネント デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NotifyIconWrapper));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItemStart = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemClose = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemStart,
+            this.toolStripMenuItemClose});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(126, 48);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // toolStripMenuItemStart
+            // 
+            this.toolStripMenuItemStart.Name = "toolStripMenuItemStart";
+            this.toolStripMenuItemStart.Size = new System.Drawing.Size(125, 22);
+            this.toolStripMenuItemStart.Text = "start/stop";
+            // 
+            // toolStripMenuItemClose
+            // 
+            this.toolStripMenuItemClose.Name = "toolStripMenuItemClose";
+            this.toolStripMenuItemClose.Size = new System.Drawing.Size(125, 22);
+            this.toolStripMenuItemClose.Text = "close";
+            this.contextMenuStrip1.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemStart;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemClose;
+    }
+}
---
+++ AugmentedKeyBoardSound/NotifyIconWrapper.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AugmentedKeyBoardSound
+{
+    public partial class NotifyIconWrapper : Component
+    {
+        public NotifyIconWrapper()
+        {
+            InitializeComponent();
+
+            // コンテキストメニューのイベントを設定
+            this.toolStripMenuItemStart.Click += this.toolStripMenuItemStartClick;
+            this.toolStripMenuItemClose.Click += this.toolStripMenuItemCloseClick;
+
+        }
+
+
+
+        public NotifyIconWrapper(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItemStartClick(object sender, EventArgs e)
+        {
+            if (KeyboardHook.IsHooking)
+            {
+                KeyboardHook.Stop();
+                return;
+            }
+
+            KeyboardHook.AddEvent(hook);
+            KeyboardHook.Start();
+        }
+
+
+        private void hook(ref KeyboardHook.StateKeyboard s)
+        {
+ 
+
+            if (s.Stroke.Equals(KeyboardHook.Stroke.KEY_DOWN))
+            {
+                if (s.inputKey.Equals(System.Windows.Input.Key.Escape))
+                {
+                    enterSoundPlayer.Play();
+                }
+                else
+                {
+                    keySoundPlayer.Play();
+                }
+            }
+        }
+
+        private System.Media.SoundPlayer keySoundPlayer = new System.Media.SoundPlayer(Properties.Resources.key);
+        private System.Media.SoundPlayer enterSoundPlayer = new System.Media.SoundPlayer(Properties.Resources.kawara);
+
+
+        private void toolStripMenuItemCloseClick(object sender, EventArgs e)
+        {
+            // 現在のアプリケーションを終了
+            Application.Current.Shutdown();
+        }
+    }
+}
